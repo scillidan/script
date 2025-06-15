74d467b37bdc90f599c33ce8099964bda5673f71
scillidan <scillidan@yandex.com>
2025-06-15 20:43:15 +0800

    Update autohotkey.bat

--- mprocs/autohotkey.bat
+++ mprocs/autohotkey.bat
@@ -1,12 +1,12 @@
 @echo off
 
 mprocs ^
-	"%HOME_DIR%\Git\dotfiles\_windows\autohotkey\User.ahk" ^
-	"%HOME_DIR%\Git\dotfiles\_windows\autohotkey\lang_user.ahk" ^
-	"%HOME_DIR%\Usr\Source\autohotkey\ahk-keyboard-locker\keyboard-locker.ahk" ^
-	"%HOME_DIR%\Usr\Source\autohotkey\AHK-KeyMap\KeyMap_User.ahk" ^
-	"%HOME_DIR%\Usr\Source\autohotkey\QuickSwitch\QuickSwitch.ahk" ^
-	"%HOME_DIR%\Usr\Source\autohotkey\ShortScript\ShortScript.ahk" ^
-	"%HOME_DIR%\Usr\Source\autohotkey\toggle-screen-autohotkey\Toggle^ screen.ahk" ^
-	"cd %HOME_DIR%\Usr\Source\autohotkey\vxdesktops.ahk && vxdesktops.ahk" ^
-	"cd %HOME_DIR%\Usr\Source\goldendict\GoldenDictOCR && GoldenDictOCR.ahk"
+	"%HOME_DIR%\Scoop\apps\autohotkey1.1\current\AutoHotkeyU64.exe %HOME_DIR%\Git\dotfiles\_windows\autohotkey\User.ahk" ^
+	"%HOME_DIR%\Scoop\apps\autohotkey1.1\current\AutoHotkeyU64.exe %HOME_DIR%\Git\dotfiles\_windows\autohotkey\lang_user.ahk" ^
+	"%HOME_DIR%\Scoop\apps\autohotkey1.1\current\AutoHotkeyU64.exe %HOME_DIR%\Usr\Source\autohotkey\ahk-keyboard-locker\keyboard-locker.ahk" ^
+	"%HOME_DIR%\Scoop\apps\autohotkey1.1\current\AutoHotkeyU64.exe %HOME_DIR%\Usr\Source\autohotkey\AHK-KeyMap\KeyMap_User.ahk" ^
+	"%HOME_DIR%\Scoop\apps\autohotkey1.1\current\AutoHotkeyU64.exe %HOME_DIR%\Usr\Source\autohotkey\QuickSwitch\QuickSwitch.ahk" ^
+	"%HOME_DIR%\Scoop\apps\autohotkey1.1\current\AutoHotkeyU64.exe %HOME_DIR%\Usr\Source\autohotkey\ShortScript\ShortScript.ahk" ^
+	"%HOME_DIR%\Scoop\apps\autohotkey1.1\current\AutoHotkeyU64.exe \"%HOME_DIR%\Usr\Source\autohotkey\toggle-screen-autohotkey\Toggle screen.ahk\"" ^
+	"cd %HOME_DIR%\Usr\Source\autohotkey\vxdesktops.ahk && %HOME_DIR%\Scoop\apps\autohotkey1.1\current\AutoHotkeyU64.exe vxdesktops.ahk" ^
+	"cd %HOME_DIR%\Usr\Source\goldendict\GoldenDictOCR && %HOME_DIR%\Scoop\apps\autohotkey1.1\current\AutoHotkeyU64.exe GoldenDictOCR.ahk"
